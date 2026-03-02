eb43e015ed1aa4bd97646c0749f34363f0229f7f
felix <felix.m.mitterer@icloud.com>
2026-03-02 23:40:41 +0100

    added card handler

---
+++ game_logic/card_handler.h
@@ -0,0 +1,22 @@
+//
+// Created by felix on 02.03.2026.
+//
+
+#ifndef BLACKJACK_CARD_HANDLER_H
+#define BLACKJACK_CARD_HANDLER_H
+
+#include "card.h"
+#include <vector>
+#include <iostream>
+
+class CardHandler {
+public:
+    std::vector<Card> cards;
+
+    CardHandler();
+
+    void print();
+};
+
+
+#endif //BLACKJACK_CARD_HANDLER_H
